79f2dee2c85c6d0dd05c223196cf6f63736ccc21
Daenur <igorden2@yandex.ru>
2022-04-06 18:52:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const selhoz_model = require('./selhoz_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
 
@@ -123,6 +123,30 @@ app.get('/country/', (req, res) => {
         })
 })
 
+app.get('/state/', (req, res) => {
+    selhoz_model.getAllstate()
+        .then(response => {
+            console.log(response);
+            res.send(response);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).send(error);
+        })
+})
+
+app.get('/atdchild/:id', (req, res) => {
+    selhoz_model.getAllChild(req.params.id.substr(1))
+        .then(response => {
+            console.log(response);
+            res.send(response);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).send(error);
+        })
+})
+
 app.get('/tableorder/:tableid.:schemaid.:order', (req, res) => {
     console.log('3');
     console.log(req.params);
@@ -218,7 +242,8 @@ app.get('/props/:tableid.:schemaid', (req, res) => {
 app.post('/insert/:text.:tableid.:schemaid', (req, res) => {
     var utc = (new Date()).toString().split(' ').splice(1,4).join(' ')
     selhoz_model.createSelhoz("'"+utc+"','"+req.params.schemaid.substr(1)+"."+req.params.tableid.substr(1)+"','INSERT','"+req.params.text.substr(1).replace(/'/g,'').replace(/,/g,";")+"','none'",'log','log_file');
-    console.log(req.params);
+ console.log('444');    
+console.log(req.params);
     selhoz_model.createSelhoz(req.params.text.substr(1),req.params.schemaid.substr(1),req.params.tableid.substr(1))
         .then(response => {
             res.status(200).send(response);
@@ -228,6 +253,19 @@ app.post('/insert/:text.:tableid.:schemaid', (req, res) => {
         })
 })
 
+app.post('/insertcus/:text.:textcol.:tableid.:schemaid', (req, res) => {
+    var utc = (new Date()).toString().split(' ').splice(1,4).join(' ')
+    selhoz_model.createSelhoz("'"+utc+"','"+req.params.schemaid.substr(1)+"."+req.params.tableid.substr(1)+"','INSERT','"+req.params.text.substr(1).replace(/'/g,'').replace(/,/g,";")+"','none'",'log','log_file');
+ console.log('444');    
+console.log(req.params);
+    selhoz_model.createcustomSelhoz(req.params.text.substr(1),req.params.textcol.substr(1),req.params.schemaid.substr(1),req.params.tableid.substr(1))
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
 
 app.delete('/delete/:id.:idname.:tableid.:schemaid', (req, res) => {
     var utc = (new Date()).toString().split(' ').splice(1,4).join(' ')
